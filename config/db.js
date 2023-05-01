771c2a6148d15d625461f7e29e2dce3be64e9120
Miguel Martínez <leugimmanre@gmail.com>
2023-05-01 20:39:53 +0200

    env

--- config/db.js
+++ config/db.js
@@ -3,18 +3,22 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const urlURI = process.env.MONGO_URI;
+const urlPORT = process.env.PORT;
+
 const connectDB = async () => {
     try {
-        const db = await mongoose.connect(process.env.MONGO_URI, {
+        const db = await mongoose.connect(urlURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
 
-        console.log(`MongoDB running on port: ${process.env.PORT}`);
-
+        console.log(`MongoDB running on port: ${urlPORT}`);
     } catch (error) {
         console.log(`error: ${error.message}`);
         process.exit(1);
     }
 }
+
+
 module.exports = connectDB;
