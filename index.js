c15e9e99aeedaaf38f0644abeca6546da6a07efa
Miguel Martínez <leugimmanre@gmail.com>
2023-05-01 20:43:29 +0200

    index.js

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(routes);
-app.use(express.urlencoded());
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
